ba92cb5cc1e271dca95c5bfa7ec65d030a7a25f9
janne022 <jannis020609@gmail.com>
2020-04-05 01:17:17 +0200

    Update Program.cs

--- Client MMORPG/Program.cs
+++ Client MMORPG/Program.cs
@@ -18,6 +18,11 @@ namespace Client_MMORPG
                 String responeseData = String.Empty;
                 int bytes = stream.Read(data2, 0, data2.Length);
                 responeseData = System.Text.Encoding.UTF8.GetString(data2, 0, bytes);
+                int length = 1 + (responeseData.Length / Console.BufferWidth);
+                Console.CursorTop -= length;
+                Console.CursorLeft = 0;
+                Console.WriteLine(new string(' ', responeseData.Length));
+                Console.CursorTop -= length;
                 Console.WriteLine(responeseData);
             }
         }
@@ -42,7 +47,7 @@ namespace Client_MMORPG
             while (true)
             {
               String messeage = username + ": " +  Console.ReadLine();
-              Byte[] data = System.Text.Encoding.ASCII.GetBytes(messeage);
+              Byte[] data = System.Text.Encoding.UTF8.GetBytes(messeage);
               stream.Write(data, 0, data.Length);
             }
         }
