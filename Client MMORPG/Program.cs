6a7af80946c86994217031eb3cd812faa0fb10c2
janne022 <jannis020609@gmail.com>
2020-04-08 00:01:16 +0200

    Update Program.cs

--- Client MMORPG/Program.cs
+++ Client MMORPG/Program.cs
@@ -18,11 +18,6 @@ namespace Client_MMORPG
                 String responeseData = String.Empty;
                 int bytes = stream.Read(data2, 0, data2.Length);
                 responeseData = System.Text.Encoding.UTF8.GetString(data2, 0, bytes);
-                int length = 1 + (responeseData.Length / Console.BufferWidth);
-                Console.CursorTop -= length;
-                Console.CursorLeft = 0;
-                Console.WriteLine(new string(' ', responeseData.Length));
-                Console.CursorTop -= length;
                 Console.WriteLine(responeseData);
             }
         }
@@ -46,9 +41,9 @@ namespace Client_MMORPG
             Console.WriteLine("You can now begin communicating");
             while (true)
             {
-              String messeage = username + ": " +  Console.ReadLine();
-              Byte[] data = System.Text.Encoding.UTF8.GetBytes(messeage);
-              stream.Write(data, 0, data.Length);
+                String message = username + ": " + Console.ReadLine();
+                Byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
+                stream.Write(data, 0, data.Length);
             }
         }
     }
